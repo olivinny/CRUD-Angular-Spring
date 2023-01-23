6c5d1fad614b16cdd2adfcac63b3890915124991
olivinny <vinicius.gtoliv@gmail.com>
2023-01-23 15:24:20 -0300

    aula 14 app spring project

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -7,6 +8,11 @@ import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [CommonModule, CoursesRoutingModule, AppMaterialModule],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule,
+    SharedModule,
+  ],
 })
 export class CoursesModule {}
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,20 +1,19 @@
 import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { delay, first, tap } from 'rxjs';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = '../../../assets/courses.json';
+  private readonly API = '/assets/courses.json';
 
   constructor(private httpClient: HttpClient) {}
 
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      delay(2000),
       tap((courses) => console.log(courses))
     );
   }
